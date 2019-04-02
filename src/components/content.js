9c8e85499d6bea8cfe06b51752442928de5cd9b5
Jared Hughes <jahughes241@gmail.com>
2019-04-01 20:30:23 -0700

    Add title, disclaimer

--- src/components/content.js
+++ src/components/content.js
@@ -3,19 +3,28 @@ import React, { Component } from "react";
 class Content extends Component {
   render() {
     return (
-      <div className="fb-content" id="content">
-        <div id="left-col">
-
+      <>
+        <div className="fb-header">
+          <h2> Facebook Generator </h2>
+          <p>
+            Build your own Facebook Feed. All graphical material is protected by the copyright owner.
+            This generator is in no way affiliated with Facebook. May only be used for personal use.
+          </p>
         </div>
-        <div id="content-col">
-          <div id="right-col">
+        <div className="fb-content" id="content">
+          <div id="left-col">
 
           </div>
-          <div id="content-area">
-            { this.props.children }
+          <div id="content-col">
+            <div id="right-col">
+
+            </div>
+            <div id="content-area">
+              { this.props.children }
+            </div>
           </div>
         </div>
-      </div>
+      </>
     )
   }
 }
